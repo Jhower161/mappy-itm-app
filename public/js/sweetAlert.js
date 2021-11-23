db2c00afce14dbe9c6e46fb696fc17455f79096e
Santiago Bedoya Santa <santiagobedoya310057@correo.itm.edu.co>
2021-11-23 01:14:16 -0500

    se agregan 3 cambios,toolbar,alerta de bienvenida,y se mueve al final de la pagina el boton de cargar aachivo para que no interfiera con la toolbar

---
+++ public/js/sweetAlert.js
@@ -0,0 +1,9 @@
+Swal.fire({
+    title: '<strong> <b>Bienvenido</b> <strong>',
+    icon: 'info',
+    
+    html:
+    'Esta es una pagina en la que podrás encontrar información acerca de proyectos de vivienda en el area metropolitana ',
+    showCloseButton: true,
+    confirmButtonText:'OK',
+  })
